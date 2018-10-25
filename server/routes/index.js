1d6da84a86eef16f0b5efcadba835237f44c09ed
Binh Nguyen <b.nguyen.sn@gmail.com>
2018-10-25 19:09:28 +0100

    Update index route to include everything (*)

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,7 +10,7 @@ const path = require('path');
  * Note that 'index.html' is served by default for the '/' route as part of our express.static configuration in app.js.
  * However, it won't be served for any other routes, hence we need to tell express to send 'index.html' here.
  * */
-router.get('/', (req, res, next) => {
+router.get('*', (req, res, next) => {
     res.sendFile('index.html', {
         root: path.join(__dirname, '../../dist'),
         maxAge: 31536000,
